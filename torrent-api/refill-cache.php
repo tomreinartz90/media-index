e1875e61565e3967a53ea0fff22aea2ebc66ac87
t.reinartz <t.reinartz@maxxton.com>
2017-12-22 13:45:23 +0100

    add sleeps and more cached files, also added the option to send a message from pushover when it has run.

--- torrent-api/refill-cache.php
+++ torrent-api/refill-cache.php
@@ -2,25 +2,63 @@
 /**
  * Simple class to get the latest information from the movie app and store this in a json file that can be used at a later stage.
  */
-require_once "../RestApi.php";
+require_once "./RestApi.php";
 
 $rest = new RestApi();
 $baseUrl = "https://torrentapi.org/pubapi_v2.php";
+$result = "Started, ";
 
 //get the token
 $token = json_decode($rest->get("$baseUrl?get_token=get_token"), true);
 $tokenKey = $token['token'];
+$result = $result . "Got a new token, ";
 
 //recent movies
-$recentMovies = $rest->get("$baseUrl?category=movies&append_to_response=videos,similar&format=json_extended&mode=list&sort=seeders&limit=100&page=1&token=$tokenKey");
+sleep(3);
+$recentMovies = $rest->get("$baseUrl?category=movies&append_to_response=videos,similar&format=json_extended&mode=list&sort=last&limit=100&page=1&token=$tokenKey");
 
-//sleep for 1 seconds due to the restriction on the api
-sleep(1);
+//recent movies
+sleep(3);
+$popularMovies = $rest->get("$baseUrl?category=movies&append_to_response=videos,similar&format=json_extended&mode=list&sort=seeders&limit=100&page=1&token=$tokenKey");
 
 //recent tv
-$recentTv = $rest->get("$baseUrl?category=tv&append_to_response=videos,similar&format=json_extended&mode=list&sort=seeders&limit=100&page=1&token=$tokenKey");
+sleep(3);
+$recentTv = $rest->get("$baseUrl?category=tv&append_to_response=videos,similar&format=json_extended&mode=list&sort=last&limit=100&page=1&token=$tokenKey");
+
+//popular tv
+sleep(3);
+$popularTv = $rest->get("$baseUrl?category=tv&append_to_response=videos,similar&format=json_extended&mode=list&sort=seeders&limit=100&page=1&token=$tokenKey");
+
+if (empty(json_decode($popularMovies, true)['error'])) {
+    file_put_contents('./popular-movies.json', $popularMovies);
+    $result = $result . "updated popular-movies, ";
+}
+if (empty(json_decode($recentMovies, true)['error'])) {
+    file_put_contents('./recent-movies.json', $recentMovies);
+    $result = $result . "updated recent-movies, ";
+}
+if (empty(json_decode($popularTv, true)['error'])) {
+    file_put_contents('./popular-tv.json', $popularTv);
+    $result = $result . "updated popular-tv, ";
+}
+if (empty(json_decode($recentTv, true)['error'])) {
+    file_put_contents('./recent-tv.json', $recentTv);
+    $result = $result . "updated recent-tv, ";
+}
+
+$result = $result . "done ";
+echo $result;
 
 
-file_put_contents('./recentMovies.json', $recentMovies);
-file_put_contents('./recentTv.json', $recentTv);
+/**
+ * Send the result to pushover to get a notification when its done.
+ */
+$pushoverToken = "";
+$pushoverUser = "";
+$pushoverMessage = [];
+$pushoverMessage['token'] = $pushoverToken;
+$pushoverMessage['user'] = $pushoverUser;
+$pushoverMessage['message'] = $result;
+$pushoverMessage['priority '] = -2;
 
+//$rest->post("https://api.pushover.net/1/messages.json", json_encode($pushoverMessage));
